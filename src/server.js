d5755712bef0c71371936842d922dd10ec0852d5
sungIn95 <dlrkehrud@naver.com>
2022-06-01 13:40:36 +0900

    #3.2정리

--- src/server.js
+++ src/server.js
@@ -4,6 +4,8 @@ const PORT = 4000;
 
 const app = express();
 
+app.get("/", () => console.log("Somebody is trying to go home."));
+
 const handleListening = () => 
   console.log(`server listenting on port http://localhost:${PORT}`);
 
