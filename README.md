b56202a2128d9db340ce790416dcd9a2da3219c3
sungIn95 <100878663+sungIn95@users.noreply.github.com>
2022-06-12 12:23:47 +0900

    #7.19 정리 fetch

--- README.md
+++ README.md
@@ -244,3 +244,7 @@ http://localhost:4500/users/github/finish 로 이동하게 된다. (처음 설�
 참고: https://github.com/settings/applications/1926230
 POST 할 곳에다가 id랑 secret를 정보화 해서 보내준다.
 그리고 fetch, DataTransfer.json() 등을 사용했는데. 솔직히 뭔지 모르겠다.
+#7.19 정리
+npm i node fetch (fetch가 사용 안되니까.)
+npm install node-fetch@2.6.1 (현재 버전에서는 안된다. )
+여기는 걍 외우는것이 답인듯하다.
--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,4 +1,5 @@
 import User from "../models/User";
+import fetch from "node-fetch";
 import bcrypt from "bcrypt";
 
 export const getJoin = (req, res) => res.render("join", { pageTitle: "Join" });
@@ -81,14 +82,27 @@ export const finishGithubLogin = async (req, res) => {
   };
   const params = new URLSearchParams(config).toString();
   const finalUrl = `${baseUrl}?${params}`;
-  const date = await fetch(finalUrl, {
-    method: "POST",
-    headers: {
-      Accept: "application/json",
-    },
-  });
-  const json = await DataTransfer.json();
-  console.log(json);
+  const tokenRequest = await (
+    await fetch(finalUrl, {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+      },
+    })
+  ).json();
+  if ("access_token" in tokenRequest) {
+    const { access_token } = tokenRequest;
+    const userRequest = await (
+      await fetch("https://api.github.com/user", {
+        headers: {
+          Authorization: `token ${access_token}`,
+        },
+      })
+    ).json();
+    console.log(userRequest);
+  } else {
+    return res.redirect("/login");
+  }
 };
 export const edit = (req, res) => res.send("Edit User");
 export const remove = (req, res) => res.send("Remove User");
