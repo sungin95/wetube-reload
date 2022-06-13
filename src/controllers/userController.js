394a8d8b11a96c2cd23cfef5a14f3b450bf67084
Sungin Lee <100878663+sungIn95@users.noreply.github.com>
2022-06-13 15:52:10 +0900

    #7.22 정리

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -44,7 +44,7 @@ export const getLogin = (req, res) =>
 export const postLogin = async (req, res) => {
   const { username, password } = req.body;
   const pageTitle = "Login";
-  const user = await User.findOne({ username });
+  const user = await User.findOne({ username, socialOnly:false });
   if (!user) {
     return res.status(400).render("login", {
       pageTitle,
@@ -116,13 +116,10 @@ export const finishGithubLogin = async (req, res) => {
     if (!emailObj) {
       return res.redirect("/login");
     }
-    const existingUser = await User.findOne({ email: emailObj.email });
-    if (existingUser) {
-      req.session.loggedIn = true;
-      req.session.user = existingUser;
-      return res.redirect("/");
-    } else {
-      const user = await User.create({
+    let user = await User.findOne({ email: emailObj.email });
+    if (!user) {
+      user = await User.create({
+        avatarUrl:userData.avatar_url, 
         name: userData.name,
         username: userData.login,
         email: emailObj.email,
@@ -130,15 +127,17 @@ export const finishGithubLogin = async (req, res) => {
         socialOnly: true,
         location: userData.location,
       });
+      } 
       req.session.loggedIn = true;
       req.session.user = user;
       return res.redirect("/");
-    }
   } else {
     return res.redirect("/login");
   }
 };
+export const logout = (req, res) => {
+  req.session.destroy();
+  return res.redirect("/");
+};
 export const edit = (req, res) => res.send("Edit User");
-export const remove = (req, res) => res.send("Remove User");
-export const logout = (req, res) => res.send("Logout");
 export const see = (req, res) => res.send("See");
--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -12,7 +12,6 @@ const userRouter = express.Router();
 
 userRouter.get("/logout", logout);
 userRouter.get("/edit", edit);
-userRouter.get("/remove", remove);
 userRouter.get("/github/start", startGithubLogin);
 userRouter.get("/github/finish", finishGithubLogin);
 userRouter.get("/:id", see);
