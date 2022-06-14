09e7e0efc8fe418ebdd166cc8a1df005dc87936e
Sungin Lee <100878663+sungIn95@users.noreply.github.com>
2022-06-15 08:54:01 +0900

    #8.1 (1)

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -10,8 +10,8 @@ import {
 
 const userRouter = express.Router();
 
-userRouter.get("/logout", logout);
-userRouter.route("/edit").get(getEdit).post(postEdit);
+userRouter.get("/logout", protectorMiddleware, logout);
+userRouter.route("/edit").all(protectorMiddleware).get(getEdit).post(postEdit);
 userRouter.get("/github/start", startGithubLogin);
 userRouter.get("/github/finish", finishGithubLogin);
 userRouter.get("/:id", see);
