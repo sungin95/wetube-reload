5d7737e1d1e51406127655271826a6e5248f1cbf
Sungin Lee <dlrkehrud@naver.com>
2022-06-22 14:48:46 +0900

    #9.1 webpack으로 할 수 있는것.

---
+++ src/client/jjs/main.js
@@ -0,0 +1,5 @@
+const hello = async () => {
+  alert("hi! Its working");
+  const x = await fetch("");
+};
+hello();
--- src/models/video.js
+++ src/models/video.js
@@ -8,7 +8,6 @@ const videoSchema = new mongoose.Schema({
   hashtags: [{ type: String, trim: true }],
   meta: {
     views: { type: Number, default: 0 },
-    rating: { type: Number, default: 0 },
   },
   owner: { type: mongoose.Schema.Types.ObjectId, required: true, ref: "User" },
 });
