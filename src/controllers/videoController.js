491a43e256220d9c79721c0c569e60e7e0aa8da3
Sungin Lee <dlrkehrud@naver.com>
2022-07-10 16:46:57 +0900

    push문제

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -10,7 +10,8 @@ export const home = async (req, res) => {
 };
 export const watch = async (req, res) => {
   const { id } = req.params;
-  const video = await Video.findById(id).populate("owner");
+  const video = await Video.findById(id).populate("owner").populate("comments");
+  console.log(video);
   if (!video) {
     return res.render("404", { pageTitle: "Video not found." });
   }
@@ -141,5 +142,8 @@ export const createComment = async (req, res) => {
     owner: user._id,
     video: id,
   });
+  video.comments.push(comment._id);
+  console.log(comment._id);
+  video.save();
   return res.sendStatus(201);
 };
