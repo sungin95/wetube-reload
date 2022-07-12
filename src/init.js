b2450c48c585d705c846c33442e59b0b52d946a5
Sungin Lee <dlrkehrud@naver.com>
2022-07-13 08:49:11 +0900

    # 16.8 댓글 삭제(만들수 있게 도와주심)

--- src/init.js
+++ src/init.js
@@ -4,7 +4,7 @@ import "./models/Video";
 import "./models/User";
 import app from "./server";
 
-const PORT = 4700;
+const PORT = 4800;
 
 const handleListening = () =>
   console.log(`server listenting on port http://localhost:${PORT}`);
