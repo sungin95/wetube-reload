2c750acda38a67c69c585ce339b0e69154d0ca54
Sungin Lee <dlrkehrud@naver.com>
2022-07-26 08:30:39 +0900

    17.2

--- webpack.config.js
+++ webpack.config.js
@@ -11,8 +11,6 @@ module.exports = {
     recorder: BASE_JS + "recorder.js",
     commentSection: BASE_JS + "commentSection.js",
   },
-  mode: "development",
-  watch: true,
   plugins: [
     new MiniCssExtractPlugin({
       filename: "css/styles.css",
